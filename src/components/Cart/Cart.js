b3c1e55795fd5ca7fc5ad12d6b93d8386e403309
Khaled Yahia <khaled.basta@protonmail.com>
2022-10-05 09:54:55 +0200

    Wrap Cart with Modal Component

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,3 +1,4 @@
+import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
 const Cart = (props) => {
   const cartItems = (
@@ -8,7 +9,7 @@ const Cart = (props) => {
     </ul>
   );
   return (
-    <div>
+    <Modal>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
@@ -18,7 +19,7 @@ const Cart = (props) => {
         <button className={classes["button--alt"]}>Close</button>
         <button className={classes.button}>Order</button>
       </div>
-    </div>
+    </Modal>
   );
 };
 
