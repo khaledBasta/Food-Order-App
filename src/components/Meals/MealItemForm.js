98d39f034bd72a030c8eb343be51f374196e1038
Khaled Yahia <khaled.basta@protonmail.com>
2022-10-05 08:38:22 +0200

    Refactor MealItemForm Component

--- src/components/Meals/MealItemForm.js
+++ src/components/Meals/MealItemForm.js
@@ -4,7 +4,17 @@ import classes from "./MealItemForm.module.css";
 const MealItemFrom = (props) => {
   return (
     <form className={classes.form}>
-      <Input />
+      <Input
+        label="Amount"
+        input={{
+          id: `amount_${props.id}`,
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
       <button>+ Add</button>
     </form>
   );
