ecfaecc0bd9eebff2b0a09df646a8b300490c079
Khaled Yahia <khaled.basta@protonmail.com>
2022-10-07 11:39:24 +0200

    Use Context to get the numberOfCartItems inisde

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Fragment, useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   const [cartIsVisible, setCartIsVisible] = useState(false);
@@ -15,13 +16,13 @@ function App() {
     setCartIsVisible(false);
   };
   return (
-    <Fragment>
+    <CartProvider>
       {cartIsVisible && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
