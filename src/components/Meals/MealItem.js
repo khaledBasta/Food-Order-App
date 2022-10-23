4fda94fe03d5e89a8538c1a0f209cbe63e52e8f4
Khaled Yahia <khaled.basta@protonmail.com>
2022-10-23 20:00:08 +0200

    Add useContext in MealItem comp

--- src/components/Meals/MealItem.js
+++ src/components/Meals/MealItem.js
@@ -1,8 +1,19 @@
+import { useContext } from "react";
 import classes from "./MealItem.module.css";
 import MealItemFrom from "./MealItemForm";
+import CartContext from "../../store/cart-context";
 
 const MealItem = (props) => {
   const price = `$${props.price.toFixed(2)}`;
+  const cartCtx = useContext(CartContext);
+  const addToCartHandler = (amount) => {
+    cartCtx.addItem({
+      id: props.id,
+      name: props.title,
+      amount: amount,
+      price: props.price,
+    });
+  };
   return (
     <li className={classes.meal} key={props.key}>
       <div>
@@ -11,7 +22,7 @@ const MealItem = (props) => {
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-        <MealItemFrom id={props.id} />
+        <MealItemFrom id={props.id} onAddToCart={addToCartHandler} />
       </div>
     </li>
   );
