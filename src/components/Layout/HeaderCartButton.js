ddbe18ba30d6739f7a9403b29973c76718071d28
Khaled Yahia <khaled.basta@protonmail.com>
2022-10-31 22:40:38 +0200

    cart button animation by Adding and Removing class

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,33 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighLighted, setBtnIsHighLighted] = useState(false);
   const cartCtx = useContext(CartContext);
 
-  const numberOfCartItems = cartCtx.items.reduce(
+  const { items } = cartCtx;
+  const numberOfCartItems = items.reduce(
     (currentNumber, item) => item.amount + currentNumber,
     0
   );
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighLighted(true);
+
+    setTimeout(() => {
+      setBtnIsHighLighted(false);
+    }, 300);
+  }, [items]);
+
+  const btnClasses = `${classes.button} ${
+    btnIsHighLighted ? classes.bump : ""
+  }`;
   return (
-    <button onClick={props.onClick} className={classes.button}>
+    <button onClick={props.onClick} className={btnClasses}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
