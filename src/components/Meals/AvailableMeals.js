9fc129c75d1820067d97aab1d2f6a98849bbed08
Khaled Yahia <khaled.basta@protonmail.com>
2022-11-26 09:36:40 +0200

    Fetch meals data

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,9 +1,39 @@
 import Card from "../UI/Card";
 import classes from "./AvailableMeals.module.css";
 import MealItem from "./MealItem";
+import useHttp from "../../hooks/use-http";
+import { useEffect, useState } from "react";
 
 const AvailableMeals = () => {
-  const MealsList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([]);
+  const { isLoading, error, sendRequest: fetchMeals } = useHttp();
+
+  useEffect(() => {
+    const dataHandler = (mealsObj) => {
+      const mealsList = [];
+      for (const key in mealsObj) {
+        if (Object.hasOwn(mealsObj, key)) {
+          const meal = mealsObj[key];
+          mealsList.push({
+            id: key,
+            description: meal.description,
+            name: meal.name,
+            price: meal.price,
+          });
+        }
+        setMeals(mealsList);
+      }
+      console.log(mealsList);
+    };
+    fetchMeals(
+      {
+        url: "https://food-order-app-eb8c1-default-rtdb.europe-west1.firebasedatabase.app/meals.json",
+      },
+      dataHandler
+    );
+  }, [fetchMeals]);
+
+  const MealsList = meals.map((meal) => (
     <MealItem
       id={meal.id}
       key={meal.id}
