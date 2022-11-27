17411dde592ff07e053a48d8e2b0ea6a2102bcbf
Khaled Yahia <khaled.basta@protonmail.com>
2022-11-27 11:17:02 +0200

    Refactor Checkout component

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,31 +1,34 @@
-import classes from "./Checkout.module.css";
+import classes from './Checkout.module.css';
 
 const Checkout = (props) => {
-  const submitHandler = (event) => {
+  const confirmHandler = (event) => {
     event.preventDefault();
   };
+
   return (
-    <form onSubmit={submitHandler}>
+    <form className={classes.form} onSubmit={confirmHandler}>
       <div className={classes.control}>
-        <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" name="userName" />
+        <label htmlFor='name'>Your Name</label>
+        <input type='text' id='name' />
       </div>
       <div className={classes.control}>
-        <label htmlFor="street">Street</label>
-        <input type="text" id="street" name="userStreet" />
+        <label htmlFor='street'>Street</label>
+        <input type='text' id='street' />
       </div>
       <div className={classes.control}>
-        <label htmlFor="postal">Postal Code</label>
-        <input type="text" id="postal" name="userPostalCode" />
+        <label htmlFor='postal'>Postal Code</label>
+        <input type='text' id='postal' />
       </div>
       <div className={classes.control}>
-        <label htmlFor="city">City</label>
-        <input type="text" id="city" name="userCity" />
+        <label htmlFor='city'>City</label>
+        <input type='text' id='city' />
       </div>
-      <button type="button" onClick={props.onCancel}>
-        Cancel
-      </button>
-      <button>Confirm</button>
+      <div className={classes.actions}>
+        <button type='button' onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button className={classes.submit}>Confirm</button>
+      </div>
     </form>
   );
 };
